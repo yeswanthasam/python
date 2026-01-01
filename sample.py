dcb2e412d400c048f30bc973392b02d6d5860f5f
yeswanthasam <asamyeswanth1434@gmail.com>
2026-01-01 13:58:27 +0530

    hello

---
+++ sample.py
@@ -0,0 +1,86 @@
+a=int(input("give a:"))
+b=int(input("give b:"))
+c=int(input("give c:"))
+
+d=(b**2) - 4*a*c
+e=d**(0.5)
+
+root1=(-b + e)/2*a
+root2=(-b - e)/2*a
+
+print(f"roots:({root1},{root2})")
+
+'''radius=int(input("radius="))
+#area=3.14*radius*radius
+#area=3.14*radius**2
+area=3.14*(radius**2)
+print("area of circle:",area)
+print("area of circle:",area,sep="")
+print(f"area of circle:{area}")
+
+num1=int(input("enter num1:"))
+num2=int(input("enter num2:"))
+print("sum:",num1+num2,sep="")
+print(f"sum:{num1+num2}")
+
+a=input()
+print("you entered:",a)
+
+a,b=input("enter a and b values: ").split(",")
+print("true and false:",a and b,"ture or false:",a or b,"not true:",not a,sep=" ")
+
+a = input("Enter first boolean (true/false): ").lower() == "true"
+b = input("Enter second boolean (true/false): ").lower() == "true"
+print(f"true and false: {a and b}, true or false: {a or b}, not true: {not a}")
+
+
+x,y=input("enter a and b values: ").split(",")
+a=int(x)
+b=int(y)
+print("addition:",a+b, "substraction:",a-b, "multipilication:",a*b, "division:",a/b)
+
+a=input()
+x,y,z=a.split()
+sum=int(x)+int(y)+int(z)
+print(sum)
+------------both are same
+x,y,z=input().split()
+sum=int(x)+int(y)+int(z)
+print(sum)
+
+
+name=input("enetr name: ")
+print("hello",name,sep=" , ",end="!")
+
+a=int(input("enter value of a"))
+b=int(input("enter value of b"))
+print(a,b,a,b,sep=",",end="ended")
+
+name=input("enter name: ")
+age=int(input("enter age: "))
+weight=float(input("enter weight: "))
+print("Hello, i am",name,"! and my age is",age,"and weight is",weight)
+
+weight_s=input("enter weight: ")
+weight=float(weight_s)
+print("weight is :",weight)
+
+name=input("enter your name: ")
+print("your name: ",name)
+
+-----(indirect)
+age_s=input("enter age: ")
+age=int(age_s)
+print("your age: ",age)
+----(direct)
+age=int(input("age :"))
+print("age is ",age)
+
+
+fruits=['appel','banana','orange']
+appel_in_fruits='banana' in fruits
+print(appel_in_fruits)
+print('-------------------------')
+grapes_not_in_fruits='jediek' not in fruits
+print(grapes_not_in_fruits)'''
+
